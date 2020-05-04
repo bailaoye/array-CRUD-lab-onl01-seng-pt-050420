94c53fa30ceef614ce3378c6c4966412d08562e2
Jeff Bai <j.bai@wustl.edu>
2020-05-04 21:48:26 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,33 @@
 def create_an_empty_array
-  
+  [ ]
 end
 
 def create_an_array
-  
+  [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  [1, 2, 3, 4] << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = [1, 2, 3]
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = [1, 2, "arrays!"]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "arrays!"]
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", 2, 3]
+  array(x)
 end
 
 def retrieve_first_element_from_array(array)
